9482553115da1fce93ea276defd32d2590c2fe17
aozeyu <3484766443@qq.com>
2022-11-06 13:04:58 +0800

    fix: commit

--- src/main/java/com/example/springbootdemo/controller/HelloController.java
+++ src/main/java/com/example/springbootdemo/controller/HelloController.java
@@ -1,2 +1,17 @@
-package com.example.springbootdemo.controller;public class HelloController {
+package com.example.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
 }
